d765a03585a689c0728b1d41aa031ed3e555ac67
24k-Baller <davidhassan194@gmail.com>
2024-04-30 11:59:16 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+// document.querySelector("p").innerHTML = "welcome to Dom";
+// document.querySelectorAll("p")[0].innerHTML = "welcome to Dom 1";
+
+// document.querySelectorAll("p")[1].innerHTML = "welcome to Dom 2";
+// document.querySelector(".result").innerHTML = "2 + 2 =4";
+
+
+function sum(
+     = document.querySelector("#num1").value;
+
+    let num2 = document.querySelector("#num2").value;
+    let total = +num1 + num2;
+   console.log("total");
+
+)
